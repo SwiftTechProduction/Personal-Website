0ca02f15d6ff5f36dc94feb72b0b3b68c09b1fce
Swift Tech Production <lukasherdianw@gmail.com>
2026-09-11 21:15:54 +0700

    Install Tailwind CSS

---
+++ Personal-Website/src/components/Hero.tsx
@@ -0,0 +1,23 @@
+function Hero()
+{
+    return (
+        <section>
+            <p>Hi, my name is Lukas</p>
+            <h1>I'm a Frontend Developer</h1>
+
+            <p>
+                I build modern, responsive, and user-friendly web applications using the latest technologies and best practices
+            </p>
+
+            <button>
+                View Portfolio
+            </button>
+
+            <div>
+                Foto nanti disini
+            </div>
+        </section>  
+    )
+}
+
+export default Hero
